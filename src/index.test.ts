21e0f0d51d862acc812ee1166542a118797e4132
jose <joseantoniolabrador90@gmail.com>
2022-04-13 19:25:04 +0200

    Single book purchase and two differents books purchase are done and tested well.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,26 @@
 import { PotterBooksOffers } from "./index"
 
+const ONE_BOOK_PRICE = 8
+const TWO_DIFFERENT_BOOKS_DISCCOUNT = 0.95
+
 describe("Potter", () => {
   it("returns price from a single book", () => {
-    const expected = 8;
+    const expected = ONE_BOOK_PRICE;
 
-    const result = PotterBooksOffers();
+    let purchase = {"bookA": 1}
 
-    expect(result).toEqual(expected);
+    const result = new PotterBooksOffers(purchase);
+
+    expect(result.totalPrice).toEqual(expected);
+  })
+
+  it("returns offer price of two different books", () => {
+    const expected = ONE_BOOK_PRICE*2*TWO_DIFFERENT_BOOKS_DISCCOUNT;
+
+    let purchase = {"bookA": 1,"bookB": 1}
+
+    const result = new PotterBooksOffers(purchase);
+
+    expect(result.totalPrice).toEqual(expected);
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-export function PotterBooksOffers(): any {
-  return 8
+import { isThisTypeNode } from "typescript"
+
+const ONE_BOOK_PRICE = 8
+const TWO_DIFFERENT_BOOKS_DISCCOUNT = 0.95
+
+export class PotterBooksOffers{
+  purchase: any
+  totalPrice: number
+
+  constructor(purchase: any){
+    
+    this.purchase = purchase
+    this.totalPrice = this.calculatePurchase()
+  }
+
+  calculatePurchase(): number{
+    
+    var total: number = 0
+
+    if (this.purchase.bookA === 1 && this.purchase.bookB === 1){
+      return ONE_BOOK_PRICE*2*TWO_DIFFERENT_BOOKS_DISCCOUNT
+    }
+
+    if (this.purchase.bookA === 1){
+      total = ONE_BOOK_PRICE
+    }
+    
+    return total
+  }
 };
